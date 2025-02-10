05411f49e902ccec510825bacd0e743f7605de21
buraito-kun <brightrock19220@outlook.co.th>
2025-02-10 19:21:20 +0700

    Fix terraform syntax

--- infra-code/variable.tf
+++ infra-code/variable.tf
@@ -1,11 +1,11 @@
 variable "image_name" {
   description = "The image name."
   type        = string
-  default     = "ghcr.io/buraito-kun/devops-assignment:"
+  default     = "ghcr.io/buraito-kun/devops-assignment"
 }
 
 variable "image_version" {
   description = "App version tag in image"
   type        = string
-  default     = "latest"
+  default     = ":latest"
 }
